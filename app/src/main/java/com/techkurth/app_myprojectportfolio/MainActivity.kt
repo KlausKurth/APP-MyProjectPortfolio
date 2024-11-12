76fd109c625cbdfebd40c8ca0482337690767626
KlausKurth <klausckurth@gmail.com>
2024-11-11 22:23:20 -0300

    FEAT transform img in circle

--- app/src/main/java/com/techkurth/app_myprojectportfolio/MainActivity.kt
+++ app/src/main/java/com/techkurth/app_myprojectportfolio/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.techkurth.app_myprojectportfolio
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.techkurth.app_myprojectportfolio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val imgprofile = BitmapFactory.decodeResource(resources,R.drawable.foto_perfil)
+        val circle = RoundedBitmapDrawableFactory.create(resources,imgprofile)
+        circle.isCircular = true
+        binding.imgProfile.setImageDrawable(circle)
+
     }
 }
