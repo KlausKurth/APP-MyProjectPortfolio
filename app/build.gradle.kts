ebd5f3516984f5c7fde79937a6679341eaa5f20a
KlausKurth <klausckurth@gmail.com>
2024-11-11 22:04:19 -0300

    FEAT main screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.techkurth.app_myprojectportfolio"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.techkurth.app_myprojectportfolio"
